750df9a2313262af479382aaa199fbb25a7d7412
Vinay Patel <vinaypatel_97@yahoo.in>
2024-05-14 00:00:07 +0100

    Added configs for uploadthing and adding logic to upload the profile photo on onboarding to uploadthing

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,6 +18,8 @@ import * as z from "zod";
 import Image from "next/image";
 import { Textarea } from "../ui/textarea";
 import { useState } from "react";
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from "@/lib/uploadthing";
 
 interface UserData {
   id: string;
@@ -35,6 +37,7 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([]);
+  const { startUpload } = useUploadThing("media");
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -46,11 +49,20 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof UserValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
-  }
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+    const blob = values.profile_photo;
+    const hasImageChanged = isBase64Image(blob);
+
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files);
+
+      if (imgRes && imgRes[0].fileUrl) {
+        values.profile_photo = imgRes[0].fileUrl;
+      }
+    }
+
+    // TODO: Update user profile.
+  };
 
   function handleImage(
     e: React.ChangeEvent<HTMLInputElement>,
